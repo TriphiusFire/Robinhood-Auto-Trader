4fcf039453f4012d1121be46761bd86d3d65521e
Jeremy Scott <terrafractis@gmail.com>
2021-01-13 20:33:34 -0800

    Update cryptoRAT.py

--- cryptoRAT.py
+++ cryptoRAT.py
@@ -34,7 +34,10 @@ def buy(symbol,dollars):
 #sell x*change worth of stock, returns the trade id
 def sell(symbol,dollars):
     print("Selling "+str(dollars)+" of "+ symbol+"...")
-    trade = r.order_sell_crypto_by_price(symbol, dollars,'bid_price','gtc')
+    if(symbol != "DOGE"):
+        trade = r.order_sell_crypto_by_price(symbol, dollars,'bid_price','gtc')
+    else:
+        trade = r.order_sell_crypto_by_quantity(symbol, int(dollars/bid(symbol)), 'bid_price', 'gtc')
     print(trade) # comment out when done testing
     return trade['id']
 
@@ -45,17 +48,33 @@ def cancel(ident):
 
 def getOrder(ident):
     order = r.get_crypto_order_info(ident)
-    print("Order state: " + order["state"])
+    #print("Order " + order)
     return order
     
 def getCryptoSymbolFromOrder(orderid):
-    s = r.get_crypto_quote_from_id(r.get_crypto_order_info('3f9b7c53-a608-455d-85bb-1220c1b20c62')['currency_pair_id'])['symbol']
+    s = r.get_crypto_quote_from_id(r.get_crypto_order_info(orderid)['currency_pair_id'])['symbol']
     if(s=="BTCUSD"): return "BTC"
     if(s=="BSVUSD"): return "BSV"
     if(s=="DOGEUSD"): return "DOGE"
     if(s=="LTCUSD"): return "LTC"
     if(s=="ETCUSD"): return "ETC"
     if(s=="ETHUSD"): return "ETH"
+    
+def countLevelsUp(previousPrice,currentPrice,factor):
+    levels = 0
+    previousPrice *= factor
+    while previousPrice < currentPrice:
+        levels+=1
+        previousPrice *= factor
+    return levels
+
+def countLevelsDown(previousPrice,currentPrice,factor):
+    levels = 0
+    previousPrice /= factor
+    while previousPrice > currentPrice:
+        levels+=1
+        previousPrice /= factor
+    return levels
 
 ############ PROGRAM ###################
     
@@ -67,8 +86,7 @@ doReal = input("\nFor mock trading just press <enter> to see which crypto *would
 if doReal.lower() == 'r': 
     realTrading = True
 
-mockSpend = 0;
-mockSell = 0;
+
 
 xBuy = 0 #dollar amount per trade, set from preferences.json
 change = 9999999 #percent change of price levels (going up), set from preferences.json
@@ -78,8 +96,8 @@ with open(preferences_file) as pdata:
     xBuy = d["xCrypto"]
     change = d["changeCrypto"]
     up = d["upCrypto"]
-xSell = xBuy*pow(change,up)
-print("\nBuying amount per level: $"+str(xBuy)+"\nSell amount per level up: $"+str(xSell))    
+
+print("\nBuying amount per level: $"+str(xBuy)) 
 
 if isfile(credentials_file):
     with open(credentials_file) as cdata:
@@ -111,6 +129,7 @@ with open(crypto_file) as json_data:
     print(json.dumps(d, indent=1))
 input("\nYour cryptos and the last trade prices of each on your account?\n\n<enter> if everything looks correct.\n")
 
+#how many symbols are being traded
 numSymbols = len(d)
 # print(numSymbols)
 
@@ -125,105 +144,118 @@ for i in range(0,numPositions):
     quantities[positions[i]["currency"]["code"]]=float(positions[i]["cost_bases"][0]["direct_quantity"])
     
 
+mockSpend = 0;
+mockSell = 0;
 index = 0
 check = {}
 #s is symbol, d is the whole rh_stock_data json dictionary
 for s in d:
-    if s in quantities:
-        print("\n"+s+" crypto sellable: "+str(quantities[s]))
-#     print("\n"+s+" last trade price: "+str(d[s]))
-    Ltp = d[s]
-#     print(s+" Ask: "+str(ask(s)))
-    Ask = ask(s)
-#     print(s+" Bid: "+str(bid(s)))
-    Bid = bid(s)
-    
-    #buying
-    if Ask < Ltp/change:
-        print("\nBuying "+s+"...\n")        
-        if realTrading:
-            orderid = (buy(s,xBuy),"buyLow")
-            time.sleep(5)
-            if getOrder(orderid[0])["state"] == "filled":
-                d[s]=Ltp/change
-                print("Buy order filled :)")
-                with open(crypto_file,'w') as outfile:
-                    json.dump(d,outfile)  
-            else:
-                if getOrder(orderid[0])["state"]=="queued": cancel(orderid[0]) 
-                check[s]=orderid
-        else: mockSpend+=xBuy
+    try:
+        if s in quantities:
+            print("\n"+s+" crypto sellable: "+str(quantities[s]))
+    #     print("\n"+s+" last trade price: "+str(d[s]))
+        Ltp = d[s]
+    #     print(s+" Ask: "+str(ask(s)))
+        Ask = ask(s)
+    #     print(s+" Bid: "+str(bid(s)))
+        Bid = bid(s)
         
-    #selling
-    if Bid > Ltp*pow(change,up): 
-        print("\nSelling "+s+" if sufficient quantity, otherwise buy...\n")
-        
-        if(s in quantities):
-            if(quantities[s]*Bid>xSell):                
-                if realTrading:
-                    orderid = (sell(s,xSell),"sellHigh")
-                    time.sleep(5)
-                    if getOrder(orderid[0])["state"] == "filled":
-                        d[s]=Ltp*change
-                        print("Sell order filled :)")
-                        with open(crypto_file,'w') as outfile:
-                            json.dump(d,outfile)  
-                    else:
-                        if getOrder(orderid[0])["state"]=="queued": cancel(orderid[0]) 
-                        check[s]=orderid
-                else: mockSell+=xSell
-            else:
-                print("Not enough crypto to sell, buying more instead...")
-                if realTrading:
-                    orderid = (buy(s,xBuy),"buyHigh")
-                    time.sleep(5)
-                    if getOrder(orderid[0])["state"] == "filled":
-                        d[s]=Ltp*pow(change,up)
-                        print("re-up Buy order filled\n")
-                        with open(crypto_file,'w') as outfile:
-                            json.dump(d,outfile)
-                    else:
-                        if getOrder(orderid[0])["state"]=="queued": cancel(orderid[0]) 
-                        check[s]=orderid
-                else: mockSpend+=xBuy  
+        #buying
+        if Ask < Ltp/change:
+            levelsDown = countLevelsDown(Ltp, Ask, change)
+            print("\nPrice has fallen by "+str(levelsDown)+" levels")
+            print("\nBuying "+str(xBuy*levelsDown)+" worth of "+s+"...\n")        
+            if realTrading:
+                orderid = (buy(s,xBuy*levelsDown),"buyLow",levelsDown)
+                time.sleep(5)
+                if getOrder(orderid[0])["state"] == "filled":
+                    d[s]=Ltp*pow(1.0/change,levelsDown)
+                    print("Buy order filled :)")
+                    with open(crypto_file,'w') as outfile:
+                        json.dump(d,outfile)  
+                else:
+                    #if getOrder(orderid[0])["state"]=="queued": cancel(orderid[0]) 
+                    check[s]=orderid
+            else: mockSpend+=xBuy*levelsDown
+            
+        #selling / price up : sell or reup buy
+        if Bid > Ltp*pow(change,up): 
+            levelsUp = countLevelsUp(Ltp, Bid, change)
+            print("\nPrice has risen by "+str(levelsUp)+" levels")
+            xSell = xBuy*pow(change,levelsUp)
+            print("\nSelling "+str(xSell)+" worth of "+s+" if sufficient quantity, otherwise buy...\n")   
+                  
+            if(s in quantities):
+                if(quantities[s]*Bid>=xSell):                
+                    if realTrading:
+                        orderid = (sell(s,xSell),"sellHigh",levelsUp)
+                        time.sleep(5)
+                        if getOrder(orderid[0])["state"] == "filled":
+                            d[s]=Ltp*change
+                            print("Sell order filled :)")
+                            with open(crypto_file,'w') as outfile:
+                                json.dump(d,outfile)  
+                        else:
+                            #if getOrder(orderid[0])["state"]=="queued": cancel(orderid[0]) 
+                            check[s]=orderid
+                    else: mockSell+=xSell
+                else: #re-up will be 5x basic buy amount for now
+                    print("\nNot enough crypto to sell, buying more instead...")
+                    if realTrading:
+                        print("\nBuying $"+str(xBuy*5)+" worth of "+s+"...") 
+                        orderid = (buy(s,xBuy*5),"buyHigh",levelsUp)
+                        time.sleep(5)
+                        if getOrder(orderid[0])["state"] == "filled":
+                            d[s]=Ask
+                            print("\nre-up Buy order filled")
+                            with open(crypto_file,'w') as outfile:
+                                json.dump(d,outfile)
+                        else:
+                            if getOrder(orderid[0])["state"]=="queued": cancel(orderid[0]) 
+                            check[s]=orderid
+                    else: mockSpend+=xBuy*5  
+    except:
+        print("\nerror\n")
+    print("\n*********************")  
                         
     
 # all the orders that were not filled 5 seconds later
 # need to check for filled before saving new LTP
 if realTrading:
-    while len(check) > 0:
-        print("Still not filled: LTP not updated so you may cancel these manually. Once all filled or cancelled this loop ends.  Trying again in 15 seconds...")
-        done = []   
-        for tup in check:
-            s = getCryptoSymbolFromOrder(tup[0])
-            print("checking: "+tup)
-            order = getOrder(tup[0])
-            if order["state"] == "filled":
-                Ltp = d[s]
-                if order["side"] == "buy": 
-                    if(tup[1]=="buyLow"):
-                        d[s]=Ltp/change
-                    if(tup[1]=="buyHigh"):
-                        d[s]=Ltp*pow(change,up)
-                if order["side"] == "sell":
-                    d[s]=Ltp*change
-                with open(crypto_file,'w') as outfile:
-                            json.dump(d,outfile) 
-                done.append(tup)
-            if order["state"] == "cancelled" or order["state"] == "canceled": 
-                done.append(tup)
-        if len(done) > 0:
-            for d in done:
-                del check[d]
-        print("Still not filled: LTP not updated so you may cancel these manually. Once all filled or cancelled this loop ends.  Trying again in 15 seconds...")
-        time.sleep(15)
-        print(check)
+    done = [] 
+    while len(check) > len(done):
+        try:
+            print("\nStill not filled: LTP not updated so you may cancel these manually. Once all filled or cancelled this loop ends.  Trying again in 15 seconds...")
+            for tup in check:
+                print(check)
+                print(check[tup][0])
+                s = getCryptoSymbolFromOrder(check[tup][0])
+                time.sleep(5);
+                order = getOrder(check[tup][0])
+                print(order);
+                if order["state"] == "filled":
+                    Ltp = d[s]
+                    if order["side"] == "buy": 
+                        if(check[tup][1]=="buyLow"):
+                            d[s]=Ltp/pow(change,check[tup][2])
+                        if(check[tup][1]=="buyHigh"):
+                            d[s]=ask(s)
+                    if order["side"] == "sell":
+                        d[s]=Ltp*change
+                    with open(crypto_file,'w') as outfile:
+                                json.dump(d,outfile) 
+                    done.append(tup)
+                if order["state"] == "cancelled" or order["state"] == "canceled" or order["state"] == "filled": 
+                    done.append(tup)        
+            print("Still not filled: LTP not updated so you may cancel these manually. Once all filled or cancelled this loop ends.  Trying again in 15 seconds...")
+            time.sleep(15)
+        except: print("\nerror...\n")
 else:
     print("\nSpend amount would be: "+str(mockSpend))
     print("Sell amount would be: "+str(mockSell))
             
-    
+print("\nDONE\n")
+
+
 #keep window open till user hits <enter>
 input()
-
-
